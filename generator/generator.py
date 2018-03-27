54cfa803eed3dce1f2a5d9408b902262d280dcb3
Aleksandr Rokashevich <rokashevich@gmail.com>
2018-03-27 14:34:00 +0300

    парсинг тестовых хедеров кроме st::types::bytes и enum'ов

--- generator/generator.py
+++ generator/generator.py
@@ -19,6 +19,20 @@ def readfile(file_path):
     return contents
 
 
+def cpp_comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return ' '
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+
 generators = []  # Сохраняем найденные во всех *.cpp вызовы json_auto_generator_, которые будем потом генерировать
 structs = {}  # Словарь вида ИМЯ_СТРУКТУРЫ:СТРУКТУРА
 
@@ -30,6 +44,9 @@ for root, dirs, files in os.walk(r'D:\tmp\cpp-to-json-generator\src'):
         file_path = os.path.join(root, name)
         if file_path.endswith('.h') or file_path.endswith('.hpp') or file_path.endswith('.cpp'):
             contents = readfile(file_path)
+            # Удаляем комментарии
+            contents = cpp_comment_remover(contents)
+
             if file_path.endswith('.cpp'):
                 matches = re.findall('json_auto_generator_(.*?\))', contents, re.DOTALL)
                 for match in matches:
@@ -38,9 +55,6 @@ for root, dirs, files in os.walk(r'D:\tmp\cpp-to-json-generator\src'):
                 lines = contents.split('\n')
                 current_struct_name = ''
                 for line in lines:
-                    # Удаляем комментарии
-                    line = re.sub('//.*?\n|/\*.*?\*/', '', line, flags=re.S)
-
                     # Пропускаем...
                     if any(x in line for x in ['return ']):
                         continue  # ...строки содержащие подстроки
@@ -57,31 +71,55 @@ for root, dirs, files in os.walk(r'D:\tmp\cpp-to-json-generator\src'):
                         if '(' in line or '};' in line:
                             current_struct_name = ''
                         elif ';' in line:  # В line теперь определение поля струтуры
-                            line = line.split(';', 1)[0].strip().replace('\t', ' ')
+                            line = line.split(';', 1)[0].strip().replace('\t', ' ').replace('{', '')
                             structs[current_struct_name].append(line)
                     elif line.startswith('typedef std::vector'):
                         structs[line.split()[-1].replace(';', '')] = line.replace('typedef ', '').replace(';', '')
-                        #line[line.find('<')+1:line.find('>')].strip()
+                    elif line.startswith('typedef st::types::byte'):
+                        structs[line.split()[-1].replace(';', '')] = line.replace('typedef ', '').replace(';', '')
 
 
-def recurse_into(struct_name):
-    global depth
-    depth += 1
-    if struct_name not in structs:
-        print('*** Не найдена стуктура %s' % struct_name)
-    tab = ' ' * depth
-    if not type(structs[struct_name]) == str:  # Нестандартный тип
-        print(tab + struct_name)
-        for param in structs[struct_name]:
-            splits = param.rsplit(' ', 1)
-            param_name = splits[1]
-            param_type = splits[0]
-            print(tab*2 + param_type + ' ' + param_name)
-    else:  # Стандартный тип
-        print(tab + struct_name+' '+structs[struct_name])
+def recurse_into(depth, fieled_type, fieled_name):
+    tab = '\t' * depth
+
+    # Сперва проверям тип на пренадлежность к стандартным
+    if fieled_type == 'unsigned int':
+        print(tab+'('+fieled_type+')'+fieled_name)
+    elif fieled_type == 'int':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'std::string':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'QString':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'unsigned long':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'char':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'unsigned char':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'float':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type == 'bool':
+        print(tab + '(' + fieled_type + ')' + fieled_name)
+    elif fieled_type.startswith('std::vector'):
+        print(tab + '[')
+        recurse_into(depth, fieled_type[fieled_type.find('<')+1:fieled_type.find('>')].strip(), fieled_name)
+        print(tab + ']')
+    elif fieled_type in structs:
+        if not type(structs[fieled_type]) == str:
+            print(tab+'['+fieled_type+']'+fieled_name)
+            for param in structs[fieled_type]:
+                child_type = param.rsplit(' ', 1)[0].strip()
+                child_name = param.rsplit(' ', 1)[1].strip()
+                recurse_into(depth+1, child_type, child_name)
+        else:
+            print(tab + fieled_type + ' ! ' + structs[fieled_type])
+    else:
+        print('*** Не найдено привило для переменной "%s" типа "%s"' % (fieled_name, fieled_type))
 
 
 for generator in generators:
-    depth = 0
     print('---')
-    recurse_into(generator.split('(')[0])
+    fieled_type = generator.split('(')[0]
+    fieled_name = generator.split('(')[1].replace(')','')
+    recurse_into(0, fieled_type, fieled_name)
