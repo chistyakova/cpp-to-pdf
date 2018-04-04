7be13e40c8df23a4438b173edfde581aa0e9f5cc
Александр Рокашевич <rokashevich@raa-st.ru>
2018-04-04 15:15:26 +0300

    добавил pdfjs 1.9.426

--- web-engine/main.cpp
+++ web-engine/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QWebEngineView>
+#include <QWebEnginePage>
 
 #include "webserver.h"
 
@@ -11,8 +12,10 @@ int main(int argc, char *argv[])
     s.start();
 
     QWebEngineView view;
-    view.setUrl(QUrl(QStringLiteral("http://localhost:8888")));
-    view.resize(1024, 750);
+    view.setUrl(QUrl(QStringLiteral("http://localhost:8888/pdfjs/web/viewer.html")));
+    //view.setUrl(QUrl(QStringLiteral("https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf")));
+    //view.setUrl(QUrl(QStringLiteral("https://mozilla.github.io/pdf.js/web/viewer.html")));
+    view.resize(1200, 1000);
     view.show();
 
     return app.exec();
