f9cb1342b1438ae299eb7a89c5807e4835d204e7
Aleksandr Rokashevich <rokashevich@gmail.com>
2018-04-05 22:22:31 +0300

    работающая связка: json -> pdfmake -> pdf.js

--- web-engine/main.cpp
+++ web-engine/main.cpp
@@ -12,10 +12,8 @@ int main(int argc, char *argv[])
     s.start();
 
     QWebEngineView view;
-    view.setUrl(QUrl(QStringLiteral("http://localhost:8888/pdfjs/web/viewer.html")));
-    //view.setUrl(QUrl(QStringLiteral("https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf")));
-    //view.setUrl(QUrl(QStringLiteral("https://mozilla.github.io/pdf.js/web/viewer.html")));
-    view.resize(1200, 1000);
+    view.setUrl(QUrl(QStringLiteral("http://localhost:8888/pdfjs/web/viewer.html?file=")));
+    view.resize(900, 500);
     view.show();
 
     return app.exec();
